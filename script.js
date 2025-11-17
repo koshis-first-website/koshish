805c9af6ca96fe7d010f65805d157b3a120d4990
koshis-first-website <koshish095@gmail.com>
2025-11-17 22:17:38 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Remove the 'document.addEventListener...' wrapper and the closing '});'
+
+const animatedTextSpan = document.querySelector('.animated-text');
+const phrases = [
+"make games",
+"create web apps",
+"prototype products",
+"develop ideas"
+];
+let phraseIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+// ... (rest of your variables) ...
+
+function typeEffect() {
+// ... (rest of your function) ...
+}
+
+// Start the animation immediately
+typeEffect();
