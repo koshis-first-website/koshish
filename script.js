e58ff45b76da01086aef537f6c8ff7b087a3764c
koshis-first-website <koshish095@gmail.com>
2025-11-18 06:18:48 +0545

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-// Remove the 'document.addEventListener...' wrapper and the closing '});'
-
 const animatedTextSpan = document.querySelector('.animated-text');
 const phrases = [
-"make games",
-"create web apps",
-"prototype products",
-"develop ideas"
+    "make games",
+    "create web apps",
+    "prototype products",
+    "develop ideas"
 ];
 let phraseIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-// ... (rest of your variables) ...
+const typingSpeed = 100;
+const deletingSpeed = 60;
+const pauseBeforeDelete = 1500;
+const pauseBeforeType = 500;
 
 function typeEffect() {
-// ... (rest of your function) ...
+    const currentPhrase = phrases[phraseIndex];
+
+    if (isDeleting) {
+        animatedTextSpan.textContent = currentPhrase.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        animatedTextSpan.textContent = currentPhrase.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    if (!isDeleting && charIndex === currentPhrase.length + 1) {
+        isDeleting = true;
+        setTimeout(typeEffect, pauseBeforeDelete);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        setTimeout(typeEffect, pauseBeforeType);
+    } else {
+        setTimeout(typeEffect, isDeleting ? deletingSpeed : typingSpeed);
+    }
 }
 
 // Start the animation immediately
